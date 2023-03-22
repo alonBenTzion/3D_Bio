a5f092b8aee3087b2f1e3dc0416d31479545284e
AlonBenZion <alon@genetikaplus.com>
2023-03-22 18:56:36 +0200

    last commit of day 1

--- 3D_ex1/main.py
+++ 3D_ex1/main.py
@@ -1,6 +1,5 @@
 import Bio.PDB
-from Bio.PDB import PDBParser, PDBList
-
+from Bio.PDB import PDBParser, PDBList, MMCIFIO
 
 def parse_pdb_file(protein_ID: str, filename: str):
     parser = PDBParser()
@@ -14,7 +13,6 @@ def get_chain(generator, chain_id):
 
 
 def get_atoms(chain_object):
-    # TODO check if we need to check 'CA'
     atoms_array = []
     for res in chain_object:
         if 'CA' in res:
@@ -28,18 +26,30 @@ def read_pdb(protein_ID: str, chain: str):
     structure = parse_pdb_file(protein_ID, filename)
     chain_object = get_chain(structure[0].get_chains(), chain)
     atoms_arr = get_atoms(chain_object)
-    return atoms_arr
+    return atoms_arr, structure
 
 
-def align(a_arr_1, a_arr_2):
+def align(a_arr_1, a_arr_2, str_1):
+    #TODO check what apply does
+    #TODO handle warnings
+
+    mmcif_io = MMCIFIO()
+    mmcif_io.set_structure(str_1)
+    mmcif_io.save("str1.cif")
     super_imposer = Bio.PDB.Superimposer()
     super_imposer.set_atoms(a_arr_1, a_arr_2)
+    super_imposer.apply(str_1.get_atoms())
     print(super_imposer.rms)
-    io = Bio.PDB.PDBIO()
-    io.set_structure(sample_structure)
-    io.save("1UBQ_aligned.pdb")
+    mmcif_io.set_structure(str_1)
+    mmcif_io.save("aligned_str.cif")
+    # io = Bio.PDB.PDBIO()
+    # io.set_structure(sample_structure)
+    # io.save("1UBQ_aligned.pdb")
+
 
 
 if __name__ == '__main__':
-    p_1_atoms_array = read_pdb('6w4h', 'A')
-    p_2_atoms_array = read_pdb('6w4h', 'A')
+    #TODO remember t change to argv1, argv2
+    p_1_atoms_array, str_1 = read_pdb('6w4h', 'A')
+    p_2_atoms_array, str_2 = read_pdb('6w4h', 'A')
+    align(p_1_atoms_array, p_2_atoms_array, str_1)
