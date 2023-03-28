8711d10ee0f48633d86c31401c59e5910fe16f57
AlonBenZion <alon@genetikaplus.com>
2023-03-28 19:06:14 +0300

    finish ex_1

--- 3D_ex1/main.py
+++ 3D_ex1/main.py
@@ -1,5 +1,7 @@
-import Bio.PDB
+import Bio.PDB as PDB
 from Bio.PDB import PDBParser, PDBList, MMCIFIO
+import sys
+
 
 def parse_pdb_file(protein_ID: str, filename: str):
     parser = PDBParser()
@@ -7,12 +9,18 @@ def parse_pdb_file(protein_ID: str, filename: str):
 
 
 def get_chain(generator, chain_id):
+    """
+    Get a specific chain
+    """
     for chain in generator:
         if chain.id == chain_id:
             return chain
 
 
 def get_atoms(chain_object):
+    """
+    Get array of 'CA' atoms from a given chain
+    """
     atoms_array = []
     for res in chain_object:
         if 'CA' in res:
@@ -20,7 +28,10 @@ def get_atoms(chain_object):
     return atoms_array
 
 
-def read_pdb(protein_ID: str, chain: str):
+def preprocess_protein(protein_ID: str, chain: str):
+    """
+    Read, parse and get list of atoms of a given protein
+    """
     pdb_list = PDBList()
     filename = pdb_list.retrieve_pdb_file(protein_ID, file_format="pdb")
     structure = parse_pdb_file(protein_ID, filename)
@@ -29,27 +40,22 @@ def read_pdb(protein_ID: str, chain: str):
     return atoms_arr, structure
 
 
-def align(a_arr_1, a_arr_2, str_1):
-    #TODO check what apply does
-    #TODO handle warnings
-
-    mmcif_io = MMCIFIO()
-    mmcif_io.set_structure(str_1)
-    mmcif_io.save("str1.cif")
-    super_imposer = Bio.PDB.Superimposer()
+def align(a_arr_1, a_arr_2, str_1, str_2):
+    """
+    Align two proteins and save each protein as cif file
+    """
+    super_imposer = PDB.Superimposer()
     super_imposer.set_atoms(a_arr_1, a_arr_2)
-    super_imposer.apply(str_1.get_atoms())
+    super_imposer.apply(a_arr_2)
+    io = MMCIFIO()
+    io.set_structure(str_1)
+    io.save(f"{str_1.id}.cif")
+    io.set_structure(str_2)
+    io.save(f"{str_2.id}.cif")
     print(super_imposer.rms)
-    mmcif_io.set_structure(str_1)
-    mmcif_io.save("aligned_str.cif")
-    # io = Bio.PDB.PDBIO()
-    # io.set_structure(sample_structure)
-    # io.save("1UBQ_aligned.pdb")
-
 
 
 if __name__ == '__main__':
-    #TODO remember t change to argv1, argv2
-    p_1_atoms_array, str_1 = read_pdb('6w4h', 'A')
-    p_2_atoms_array, str_2 = read_pdb('6w4h', 'A')
-    align(p_1_atoms_array, p_2_atoms_array, str_1)
+    p_1_atoms_array, str_1 = preprocess_protein(sys.argv[1], sys.argv[2])
+    p_2_atoms_array, str_2 = preprocess_protein(sys.argv[3], sys.argv[4])
+    align(p_1_atoms_array, p_2_atoms_array, str_1, str_2)
